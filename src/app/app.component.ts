48e67592ec0d25f08d8df35fb88270dd298ae198
nandini <nandani@contaque.com>
2025-05-09 12:33:45 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'routing17';
+  // ROUTING MEANS WAY KE EK PAGE SE DUSRE ME JANE KE
+  // iske liye sabse phale app.routes.ts me jaooo
+
 }
